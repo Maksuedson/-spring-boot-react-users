614284ec1f768bbfc61f0849ae0a481224e3780a
Maksuedson <maksuedson@hotmail.com>
2021-03-31 18:34:12 -0300

    add env

--- Front-web/src/service/ApiService.js
+++ Front-web/src/service/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = 'https://react-usuario-backend-spring.herokuapp.com';
+const USER_API_BASE_URL = process.env.REACT_APP_API_URL
 
 class ApiService {
 
@@ -17,7 +17,7 @@ class ApiService {
     }
 
     addUser(user) {
-        return axios.post(""+`${USER_API_BASE_URL}/users`, user);
+        return axios.post(`${USER_API_BASE_URL}/users`, user);
     }
 
     editUser(user) {
