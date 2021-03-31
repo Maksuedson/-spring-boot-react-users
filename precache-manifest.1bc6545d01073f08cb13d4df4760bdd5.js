bc9700e895acc84edb8506e33fc0fa71b926691d
Maksuedson <maksuedson@hotmail.com>
2021-03-31 20:43:07 -0300

    Updates

---
+++ precache-manifest.1bc6545d01073f08cb13d4df4760bdd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde93943b0076fec07dfaaf31c926f99",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c780e6726b91888dacc5",
+    "url": "/my-app/static/css/main.9a87454c.chunk.css"
+  },
+  {
+    "revision": "4fd363ec43f8c5964674",
+    "url": "/my-app/static/js/2.c58b1524.chunk.js"
+  },
+  {
+    "revision": "c780e6726b91888dacc5",
+    "url": "/my-app/static/js/main.36db76be.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
