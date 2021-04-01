10e3ec36416b648b4bbab4191c6535aa467a104e
Maksuedson <maksuedson@hotmail.com>
2021-03-31 21:05:30 -0300

    Updates

---
+++ precache-manifest.4f45271578cc666aed04b1336eded211.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a3d5b5c24bf1ebaa03bd9618b1726e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e01cf0e75ed97896dc0",
+    "url": "/static/css/main.9a87454c.chunk.css"
+  },
+  {
+    "revision": "abf8d05a976906cbde7b",
+    "url": "/static/js/2.5c7bde4d.chunk.js"
+  },
+  {
+    "revision": "3e01cf0e75ed97896dc0",
+    "url": "/static/js/main.57c4964c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spring-boot-react-users/precache-manifest.ca0fabfd68c86bf97af347a60395804e.js"
+  "/precache-manifest.4f45271578cc666aed04b1336eded211.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spring-boot-react-users/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
